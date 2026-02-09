e35640731d82f422df73a2b78486d621f677d3f3
GauravRaj-07` <gauravrajgr504366@gamil.com`>
2026-02-09 14:29:02 +0530

    syncing auth and db

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,22 @@ import express from "express";
 import path from "path";
 import cors from "cors";
 import { connectDB } from "./lib/db.js";
+import {serve} from "inngest/express"
 
 import { ENV } from "./lib/env.js";
+import { inngest,functions } from "./lib/inngest.js";
 
 const app = express();
 
+// middleware
+app.use(express.json());
+// credentials: true means =>server allows a browser to include cookies on request
 app.use(cors({
-    // origin: [
-    //   "http://localhost:5173",              // local frontend
-    //   "https://your-frontend.vercel.app"    // deployed frontend
-    // ],
-    origin: true,
+    origin: ENV.CLIENT_URL,
     credentials: true
-  }));
+}));
+
+app.use("/api/inngest",serve({client:inngest,functions}))
 
 const __dirname = path.resolve();
 
